c88355714f0a58fd427a29993019710f1aa1e5dd
tinjapup <tinjapup@metropolia.fi>
2025-01-16 15:11:43 +0200

    update package.json

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-// index.js
-import http from 'http';
+import express from 'express';
 const hostname = '127.0.0.1';
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to my REST API!');
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
