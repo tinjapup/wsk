5c5f7d9b2e31887ebdc9e3e96ea6e14588872da9
tinjapup <tinjapup@metropolia.fi>
2025-01-22 15:04:40 +0200

    Fixing and added new features

--- src/main.js
+++ src/main.js
@@ -4,55 +4,76 @@ const app = express();
 const port = 3000;
 
 
-
 // staattinen html-sivusto tarjoillaan palvelimen juuressa
 app.use('/', express.static('public'));
+
 
 // middleware, joka lukee json-datan POST pyyntöjen rungosta
 app.use(express.json());
 
+
 // rest-apin resurssit tarjoillaan /api/-polun alla
 app.get('/api/', (req, res) => {
   res.send('Welcome to my GET API!');
+  res.status(201);
   console.log(req.url);
   console.log('mooooi');
 });
 
 app.get('/api/moro', (req, res) => {
   console.log(req.body);
-  res.json({reply: 'moroiii!'} + req.body.sender);
+  res.status(201);
+  res.json({reply: 'moro!' + req.body.sender});
 });
 
+// reading from route params
 app.get('/api/sum/:num1/:num2', (req, res) => {
   console.log(req.params);
+
   const num1 = parseInt(req.params.num1);
   const num2 = parseInt(req.params.num2);
-  console.log(num1, num2);
+
+  // testing that both parameters are numbers
+  if(isNaN(num1) || isNaN(num2)) {
+    res.status(400);
+    res.json({
+      error: 'Both parameters must be numbers!'
+    });
+    return;
+  }
 
   res.json({
     num1,
     num2,
     result: num1 + num2,
   });
+
 });
 
-
-app.get('/api/sum/:num1/:num2', (req, res) => {
+// square of a number, test
+app.get('/api/square/:num', (req, res) => {
   console.log(req.params);
-  const num1 = parseInt(req.params.num1);
-  const num2 = parseInt(req.params.num2);
-  console.log(num1, num2);
 
+  const num = parseInt(req.params.num);
+
+  if (isNaN(num)) {
+    res.status(400);
+    res.json({
+      error: "Parameter must be a number!"
+    });
+    return;
+  }
   res.json({
-    num1,
-    num2,
-    result: num1 + num2,
+    num,
+    squareOfNumber: num * num,
   });
 });
 
-
+// moro
 app.post('/api/moro', (req, res) => {
-  res.send('Welcome to my POST API!');
+  res.status(200)
+  console.log(req.body);
+  res.json({reply: 'no Moro ' + req.body.sender});
 });
 
 app.listen(port, hostname, () => {
